5d592b557c5545d01d71a0a0d2d37e1178deaf9d
aiden1yoon <46219072+aidenjyoon@users.noreply.github.com>
2021-12-08 12:22:26 -0800

    try saving image with torchvision only

--- dual-inputs/vutil.py
+++ dual-inputs/vutil.py
@@ -5,11 +5,13 @@ import numpy as np
 
 
 def save_image(tensor, filename):
-    if tensor.size()[0] == 1:
-        tensor = tensor.cpu()[0, ...]
-        ndarr = tensor.mul(255).clamp(0, 255).byte().permute(1, 2, 0).numpy()
-        im = Image.fromarray(ndarr)
-        im.save(filename)
-    else:
-        torchvision.utils.save_image(
+    # if tensor.size()[0] == 1:
+    #     tensor = tensor.cpu()[0, ...]
+    #     ndarr = tensor.mul(255).clamp(0, 255).byte().permute(1, 2, 0).numpy()
+    #     im = Image.fromarray(ndarr)
+    #     im.save(filename)
+    # else:
+    #     torchvision.utils.save_image(
+    #         tensor, filename, normalize=False, range=(0, 1))
+    torchvision.utils.save_image(
             tensor, filename, normalize=False, range=(0, 1))
