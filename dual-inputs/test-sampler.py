c9c6f1278d1afb260c9172d9f980fc95be689581
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2021-12-23 21:38:10 -0800

    Update test-sampler.py

--- dual-inputs/test-sampler.py
+++ dual-inputs/test-sampler.py
@@ -18,7 +18,7 @@ import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
 import torch.optim as optim
-import torch.utils.data import Dataset
+import torch.utils.data as Dataset
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
