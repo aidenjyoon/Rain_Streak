d2497498472effcb19200debb6b444df0ecbdde0
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2022-02-16 18:55:16 -0800

    .

---
+++ rain_generator/util.py
@@ -0,0 +1,9 @@
+# to change the image name to 1 to n same as the trainin datasets name for single image training
+def changeImgName(images_dataset):
+    for idx, image in enumerate(images_dataset):
+
+
+def __main__():
+    images_dataset = "../../datasets/imagenet/ILSVRC/Data/CLS-LOC/train/n02219486"
+    
+    changeImgName(images_dataset)
